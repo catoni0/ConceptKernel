bd31e6a3ad0826040661b7097faacc4c714d71e3
vovanx500 <mr.vladimir1337@gmail.com>
2015-09-23 21:19:31 +0300

    new config file

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,7 +3,7 @@
 
 export CROSS_COMPILE=/home/dev/android/arm-eabi-4.9/bin/arm-eabi-
 
-make ARCH=arm CROSS_COMPILE=$CROSS_COMPILE rhine_togari_row_defconfig
+make ARCH=arm CROSS_COMPILE=$CROSS_COMPILE zultra_defconfig
 make ARCH=arm CROSS_COMPILE=$CROSS_COMPILE clean
 
 
